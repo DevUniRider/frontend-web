35d27e75bd35c3b01ecd7fd2939f40fb293ab5ed
Nicolas <nixl20@github.com>
2024-05-01 02:42:03 -0500

    feat(register-user): implement register-user component, add imagen logoUniRider & change the style.css

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,36 @@
-<script setup>
+<script>
 import MapCard from './unirider/shared/components/map-card/map-card.component.vue';
 import TravelInfoCard from './unirider/shared/components/travel-info-card/travel-info-card.component.vue';
 import LoginUser from './unirider/shared/pages/login-user.component.vue';
+import RegisterUser from './unirider/shared/pages/register-user.component.vue';
 
+export default {
+  name: "app",
+  components: {LoginUser, RegisterUser},
+  data() {
+    return {
+      userName: '',
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    onUserRegistered(user) {
+      this.userName = user.userName;
+      this.email = user.email;
+      this.password = user.password;
+      console.log("User registered: ", user);
+    },
+  }
+
+}
 
 </script>
 
 <template>
-  <login-user/>
+  <!--login-user/-->
+  <!--register-user v-on:user-registered="onUserRegistered"/-->
   <!--<MapCard />-->
-  <TravelInfoCard />
+  <!--TravelInfoCard /-->
 </template>
 
