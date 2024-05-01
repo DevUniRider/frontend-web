4af15fc21d9754edd891cbc2d66bcc2d200e627b
H4RRY73 <103443967+H4RRY73@users.noreply.github.com>
2024-05-01 12:08:13 -0500

    fix(map):update travel information.

--- src/unirider/shared/components/travel-info-card/travel-info-card.component.vue
+++ src/unirider/shared/components/travel-info-card/travel-info-card.component.vue
@@ -1,42 +1,49 @@
 <script>
+import AutoImage from '../../../../assets/auto.jpg';
 export default {
   name: 'TravelInfoCard',
+  data() {
+    return {
+      autoImage: AutoImage
+    }
+  }
 }
 </script>
 
 <template>
-    <div class="travel-info-card">
-      <table>
-        <tr>
-          <th colspan="2"><h2>Información del viaje</h2></th>
-        </tr>
-        <tr>
-          <td>Distancia:</td>
-          <td>9 KM</td>
-        </tr>
-        <tr>
-          <th colspan="2"><h2>Conductor disponible</h2></th>
-        </tr>
-        <tr>
-          <td>Punto de recojo:</td>
-          <td>Av. Gral. Salaverry, Jesús María 15072</td>
-        </tr>
-        <tr>
-          <td rowspan="2"><img src="../../../assets/auto.jpg"></td>
-          <td>KIA-CERATO</td>
-        </tr>
-        <tr>
-          <td>A1B-234</td>
-        </tr>
-        <tr>
-          <td colspan="2">
-            <span class="driver-name">Juan Pérez</span>
-            <span class="driver-rating">4.5</span>
-          </td>
-        </tr>
-      </table>
-      <button class="request-trip-button">Solicitar viaje</button>
-    </div>
+  <div class="travel-info-card">
+    <table>
+      <tr>
+        <th colspan="2"><h2>Información del viaje</h2></th>
+      </tr>
+      <tr>
+        <td>Distancia:</td>
+        <td>9 KM</td>
+      </tr>
+      <tr>
+        <th colspan="2"><h2>Conductor disponible</h2></th>
+      </tr>
+      <tr>
+        <td>Punto de recojo:</td>
+        <td>Av. Gral. Salaverry, Jesús María 15072</td>
+      </tr>
+      <tr>
+        <td rowspan="2"><img class="auto-image" :src="autoImage" alt="Auto"/></td>
+        <td>KIA-CERATO</td>
+      </tr>
+      <tr>
+        <td>A1B-234</td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <span class="driver-name">Juan Pérez</span>
+          <span class="driver-rating">4.5</span>
+          <span class="star-icon">⭐</span>
+        </td>
+      </tr>
+    </table>
+    <button class="request-trip-button">Solicitar viaje</button>
+  </div>
 </template>
 
 <style>
@@ -48,6 +55,16 @@ export default {
 }
 .travel-info-card table td {
   text-align: center;
+}
+.auto-image {
+  width: 70px;
+  height: auto;
+}
+.driver-name {
+  margin-right: 10px;
+}
+.star-icon {
+  margin-left: 5px;
 }
 .request-trip-button {
   background-color: #39BFBF;
