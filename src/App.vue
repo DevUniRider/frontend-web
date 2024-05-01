f7967a5a2c8d9ef3f93d9665663204e83ceba40a
Nicolas <nixl20@github.com>
2024-05-01 02:04:57 -0500

    feat(login): implement login component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import MapCard from './unirider/shared/components/map-card/map-card.component.vue';
 import TravelInfoCard from './unirider/shared/components/travel-info-card/travel-info-card.component.vue';
+import LoginUser from './unirider/shared/pages/login-user.component.vue';
+
+
 </script>
 
 <template>
+  <login-user/>
   <!--<MapCard />-->
   <TravelInfoCard />
 </template>
