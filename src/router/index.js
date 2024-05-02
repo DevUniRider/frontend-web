e20f7d6c6d991195f8c9ee4e9c3d4e06ffcdbb35
Nicolas <nixl20@github.com>
2024-05-02 01:32:15 -0500

    feat(index.js): add more paths to router components

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,24 @@
-import {createRouter, createWebHistory} from "vue-router";
-import SuscriptionPagoComponent from "../unirider/subscription/pages/suscription-pago.component.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import QualifyCard from '../unirider/shared/components/qualify-card/qualify-card.component.vue';
+import MapsPage from "../unirider/shared/pages/maps-page.component.vue";
+import LoginUser from '../unirider/shared/pages/login-user.component.vue';
+import ToolbarComponent from "../unirider/public/toolbar.component.vue";
+import Home from "../unirider/shared/pages/home.component.vue";
+import RegisterUser from "../unirider/shared/pages/register-user.component.vue";
+const routes = [
+    { path: '/', component: LoginUser },
+    { path: '/create', component: RegisterUser },
+    { path: '/home', name: 'Home', component: Home },
+    { path: '/main', name:'main', component: ToolbarComponent },
+    { path: '/maps', component: MapsPage },
+    { path: '/qualify', component: QualifyCard },
+
+
+]
+
 const router = createRouter({
     history: createWebHistory(),
-    routes: [
-        { path: '/payment',    component: SuscriptionPagoComponent},
-        { path: '/',        redirect: '/home'}
-    ]
-});
+    routes
+})
 
-export default router;
+export default router
