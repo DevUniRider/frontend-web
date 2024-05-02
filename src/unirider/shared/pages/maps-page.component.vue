fc33ed664a536ce3d8846a7bbd3542de9dffe043
H4RRY73 <103443967+H4RRY73@users.noreply.github.com>
2024-05-01 23:13:58 -0500

    feat(maps-page):add maps page component

---
+++ src/unirider/shared/pages/maps-page.component.vue
@@ -0,0 +1,23 @@
+<script setup>
+import MapCard from '../components/map-card/map-card.component.vue';
+import TravelInfoCard from '../components/travel-info-card/travel-info-card.component.vue';
+</script>
+<template>
+  <div class="card-container">
+    <MapCard />
+    <TravelInfoCard />
+  </div>
+</template>
+
+
+<style scoped>
+.card-container {
+  display: flex;
+  justify-content: space-between;
+}
+@media (min-width: 768px) {
+  .card-container {
+    flex-direction: row;
+  }
+}
+</style>
